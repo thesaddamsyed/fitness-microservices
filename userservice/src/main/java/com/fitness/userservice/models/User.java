c61e875c51060427fd26651b8dcf4587c84a71bb
thesaddamsyed <thesaddamsyed@gmail.com>
2025-07-21 13:03:09 +0530

    finished setting the base user service microservice , created a user entity and successfully able to connect it with the postgres  database

---
+++ userservice/src/main/java/com/fitness/userservice/models/User.java
@@ -0,0 +1,42 @@
+package com.fitness.userservice.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
