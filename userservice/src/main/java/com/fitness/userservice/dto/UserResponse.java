5f2b224132cca1797b17e851121e1d2bd38329c6
thesaddamsyed <thesaddamsyed@gmail.com>
2025-07-22 18:43:06 +0530

    created both the DTOs

---
+++ userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
@@ -0,0 +1,18 @@
+package com.fitness.userservice.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+
+    private String Id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
