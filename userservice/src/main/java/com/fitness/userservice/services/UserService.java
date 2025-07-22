698142f92ccf26a12186056c8ff763facc3f81ac
thesaddamsyed <thesaddamsyed@gmail.com>
2025-07-22 18:42:14 +0530

    created service class reposible for all kinds of operations on users

---
+++ userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -0,0 +1,75 @@
+package com.fitness.userservice.services;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.models.User;
+import com.fitness.userservice.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public UserResponse register(RegisterRequest request) {
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+        User savedUser = repository.save(user);
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId().toString());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+
+        return userResponse;
+
+    }
+
+    // getting all users
+
+    public List<UserResponse> getUsers() {
+        List<User> users = repository.findAll();
+        return users.stream().map(user -> {
+            UserResponse response = new UserResponse();
+            response.setId(user.getId().toString());
+            response.setEmail(user.getEmail());
+            response.setFirstName(user.getFirstName());
+            response.setLastName(user.getLastName());
+            response.setCreatedAt(user.getCreatedAt());
+            response.setUpdatedAt(user.getUpdatedAt());
+            return response;
+        }).collect(Collectors.toList());
+    }
+
+    public UserResponse getUserProfile(String id) {
+        User user = repository.findById(UUID.fromString(id))
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId().toString());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+
+        return userResponse;
+    }
+
+}
