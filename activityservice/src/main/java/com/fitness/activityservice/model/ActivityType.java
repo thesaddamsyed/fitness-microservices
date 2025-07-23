f792a5a2368f702b5116c24b320eb3740f8b004b
thesaddamsyed <thesaddamsyed@gmail.com>
2025-07-23 22:19:48 +0530

    defined the properties of activity class and created an enum

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,27 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    SWIMMING,
+    CYCLING,
+    YOGA,
+    WEIGHT_LIFTING,
+    HIIT,
+    DANCE,
+    AEROBICS,
+    PILATES,
+    HIKING,
+    ROCK_CLIMBING,
+    BOXING,
+    MARTIAL_ARTS,
+    SKIING,
+    SNOWBOARDING,
+    GOLF,
+    TENNIS,
+    BASKETBALL,
+    SOCCER,
+    BASEBALL,
+    VOLLEYBALL,
+    CRICKET;
+}
