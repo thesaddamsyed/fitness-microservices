5f47b49c960b0e0c0a4a3973bc8fb25c7dfc6718
thesaddamsyed <thesaddamsyed@gmail.com>
2025-08-04 12:22:10 +0530

    created a representation class for reciieveing the data from the rabbitmq queue as message payload

---
+++ aiservice/src/main/java/com/fitness/aiservice/model/Activity.java
@@ -0,0 +1,18 @@
+package com.fitness.aiservice.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import lombok.Data;
+
+@Data
+public class Activity {
+    private String id;
+    private String userId;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Map<String, Object> additionalMetrics;
+}
