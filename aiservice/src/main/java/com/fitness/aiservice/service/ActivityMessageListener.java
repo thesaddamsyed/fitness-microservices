c5a45081f015bd7cc639c1b171237da6c8ddd487
thesaddamsyed <thesaddamsyed@gmail.com>
2025-08-05 15:43:56 +0530

    saves the recommendations to the repository

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +15,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ActivityMessageListener {
 
+    private final ActivityAiService aiService;
+    private final RecommendationRepository recommendationRepository;
+
     @RabbitListener(queues = "fitness.queue")
     public void processActivity(Activity activity) {
         log.info("recieved activity for processing: {}", activity.getId());
+        log.info("Generating recommendation............");
+        // aiService.generateRecommendation(activity));
+        Recommendation recommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
